6b692aaabbbb8c778462a5e74051b0c30a5189b8
kevin <kevinelsen@hotmail.fr>
2018-06-25 10:51:33 +0200

    exo4

---
+++ script.js
@@ -0,0 +1,7 @@
+/*Déclaration de la fonction*/
+function aie() {
+/*Déclaration des variables*/
+  var lastname = document.getElementById('lastname').value ='';
+  var firstname = document.getElementById('firstname').value = '';
+  var city = document.getElementById('city').value = '';
+}
